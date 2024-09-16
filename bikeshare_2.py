e33fb2ec64ac5626e61fcc005597a5b2c7fb165f
Xavi <SBUDI01@EsseatPfs.net>
2024-09-16 20:43:57 +0200

    The filters section is commented, detailing what is going to be filtered

--- bikeshare_2.py
+++ bikeshare_2.py
@@ -5,7 +5,7 @@ import numpy as np
 CITY_DATA = { 'chicago': 'chicago.csv',
               'new york city': 'new_york_city.csv',
               'washington': 'washington.csv' }
-
+# filters city, month and day
 def get_filters():
     """
     Asks user to specify a city, month, and day to analyze.
